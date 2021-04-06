34b1dbc7234bad125b8dd7b66c4ea5103c79cdda
Jeronimo Kulandissa <zemiro014@gmail.com>
2021-04-05 21:49:00 -0300

    Mostrando como se pode adicionar dinamicamente os objectos

--- src/br/com/dominio/Programa.java
+++ src/br/com/dominio/Programa.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 import br.com.dominio.model.Produto;
 import br.com.dominio.service.CalculoPreco;
+import br.com.dominio.service.CalculoPrecoComBrinde;
 import br.com.dominio.service.CalculoPrecoComFrete;
 import br.com.dominio.service.CalculoPrecoComum;
 
@@ -17,7 +18,11 @@ public class Programa {
 		produto.setQuantidade(1);
 		produto.setValor(new BigDecimal("40.00"));
 		
-		CalculoPreco calculo = new CalculoPrecoComFrete(new CalculoPrecoComum());
+		/*
+		 *  Adicionando dinâmicamente os Objectos -> a partir do preco comum, pode se adicionar frete e brind de forma dinâmica. Adicionando os objectos. 
+		 *  Isso só é possívelo pois aas classes (CalculoPrecoComBrinde; CalculoPrecoComFrete e CalculoPrecoComum) são subClasses da mesma interface
+		 * */ 
+		CalculoPreco calculo = new CalculoPrecoComBrinde (new CalculoPrecoComFrete(new CalculoPrecoComum()));
 		
 		BigDecimal preco = calculo.valorFinal(produto);
 		
